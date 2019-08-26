93d18836d4be8ae41c3ba9397e53fa5f48421501
Matthew632 <matthew.hebb@hotmail.co.uk>
2019-08-26 14:14:14 +0100

    adds redux and react

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,23 @@
-console.info('React is cool');
+import App from './App';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import getStore from './getStore';
+import { Provider} from 'react-redux';
+
+const store = getStore();
+
+const fetchDataForLocation = () => {
+    store.dispatch({type:'REQUEST_FETCH_QUESTIONS'})
+};
+
+const render = (_App) => {
+    ReactDOM.render(
+        <Provider store={store}>
+        <_App />
+        </Provider>,
+        document.getElementById('AppContainer')
+    )
+};
+
+render(App);
+fetchDataForLocation();
